c4dcce97dd90fff2452100cbf1b3f8b4d80b5735
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-10 11:47:03 -0300

    feat: parameter name received and saved with MongoDB, in addition login status message

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+try {
+  await mongoClient.connect();
+  db = mongoClient.db("batePapoUol");
+} catch (err) {
+  console.log(err);
+}
+
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+
+  const newParticipant = {
+    name,
+    lastStatus: Date.now(),
+  };
+
+  console.log(newParticipant.lastStatus);
+
+  const loginStatus = {
+    from: newParticipant.name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: dayjs(newParticipant.lastStatus).format("HH:mm:ss"),
+  };
+
+  try {
+    await db.collection("participants").insertOne(newParticipant);
+    await db.collection("messages").insertOne(loginStatus);
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(5000, () => {
+  console.log(`Server running in port: ${5000}`);
+});
